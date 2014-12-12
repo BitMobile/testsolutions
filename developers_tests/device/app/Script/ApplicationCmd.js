56b944ac4d372537fac24590e8bbd3eb8deedb8e
Kugushev Aleksandr <kugushew@gmail.com>
2014-12-12 13:11:23 +0300

    [PLA-108] Add tests for logs

--- developers_tests/device/app/Script/ApplicationCmd.js
+++ developers_tests/device/app/Script/ApplicationCmd.js
@@ -5,3 +5,22 @@ function Exit() {
 function LogOut(){
 	Application.Logout();	
 }
+
+function SendLog()
+{
+	var result = Application.SendDatabase();
+	if(result)
+		Dialog.Message("Success");
+	else
+		Dialog.Message("Fail");
+}
+
+function ClearLog()
+{
+	Application.ClearLog();
+}
+
+function DBSize()
+{
+	return DB.Size.ToString();
+}
--- developers_tests/device/app/Script/ScrollView.js
+++ developers_tests/device/app/Script/ScrollView.js
@@ -3,8 +3,22 @@ function OnLoad()
 	$.sv.Index = parseInt($.edtIndex.Text);
 }
 
+function NullOrDefault(value, defaultValue)
+{
+	if(value == null)
+		return defaultValue;
+	else
+	    return value;
+}
+
 function Scroll(sender, text)
 {	
 	var index = parseInt(text);
 	$.sv.Index = index;
+}
+
+function RefreshAndScroll(sender, text)
+{
+	var index = parseInt(text);
+	DoRefresh(index)
 }
