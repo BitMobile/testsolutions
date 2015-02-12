19c98736fc55d029a072a3d31e369d9f298da73b
Kugushev Aleksandr <kugushew@gmail.com>
2015-02-12 13:09:59 +0300

    [PLA-251][PLA-353] Add tests for new DB features

--- developers_tests/device/app/Script/Database.js
+++ developers_tests/device/app/Script/Database.js
@@ -1,20 +1,56 @@
-
-function Create(){
+function Create() {
 	var entity = DB.Create("Catalog.Stock");
 	entity.Save();
 	DoRefresh();
 }
 
-function DeleteLast(){
+function DeleteLast() {
 	var query = new Query("SELECT Id FROM Catalog_Stock");
-	if(query.ExecuteCount() > 0){
+	if (query.ExecuteCount() > 0) {
 		var ref = query.ExecuteScalar();
 		DB.Delete(ref, false);
-		DoRefresh();	
+		DoRefresh();
 	}
 }
 
-function Select(){
+function Select() {
 	var query = new Query("SELECT * FROM Catalog_Stock");
 	return query.Execute();
+}
+
+function CreateUserTable(sender, name) {
+	if (name != null && !IsBlankString(name)) {
+		DB.CreateTable(name, [ "Text" ]);
+		DoRefresh(name);
+	}
+}
+
+function ClearUserTable(sender, name) {
+	if (name != null && !IsBlankString(name)) {
+		DB.TruncateTable(name);
+		DoRefresh(name);
+	}
+}
+
+function DropUserTable(sender, name) {
+	if (name != null && !IsBlankString(name)) {
+		DB.DropTable(name);
+		DoRefresh();
+	}
+}
+
+function InsertUserTable(sender, name, value) {
+	if (name != null && !IsBlankString(name)) {		
+		var q = new Query("VALUES('" + value + "')");
+		q.ExecuteInto(name);
+		DoRefresh(name);
+	}
+}
+
+function SelectUserTable(name) {
+	if (name != null && !IsBlankString(name)) {
+		var q = new Query("SELECT * FROM UT_" + name);
+		return q.Execute();
+	}
+	return [];
 }
