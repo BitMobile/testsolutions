70729e9d16f8f99618a5dbc3b21982c209994d23
Kugushev Aleksandr <kugushew@gmail.com>
2015-05-05 12:47:53 +0300

    [PLA-397] Add checking of getType()

--- developers_tests/device/app/Script/ScriptEngine.js
+++ developers_tests/device/app/Script/ScriptEngine.js
@@ -17,3 +17,10 @@ function StringFormat(){
 function NewFormat(){
 	return Format("String format: {0} {1}!", "success", ToString(42))
 }
+
+function GetType(){
+	var query = new Query("SELECT Id FROM Catalog_User LIMIT 1");
+	var ref = query.ExecuteScalar();
+	var obj = ref.GetObject();
+	return getType(obj);
+}
