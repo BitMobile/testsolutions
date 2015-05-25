5f8a2ea414940c0f075dcfd861ce3b57afa191d5
Kugushev Aleksandr <kugushew@gmail.com>
2015-05-25 17:31:15 +0300

    [PLA-529] Add ClientModel.HttpRequest

---
+++ developers_tests/device/app/Script/ClientModel/ClientModel_HttpRequest.js
@@ -0,0 +1,42 @@
+var headers = [];
+
+function HttpAddHeader(controlName, controlValue) {
+	headers.push([ controlName.Text, controlValue.Text ]);
+	controlName.Text = "";
+	controlValue.Text = "";
+}
+
+function HttpGet(address, query) {
+	try {
+		request = CreateRequest(address);
+		var result = request.Get(query);
+		Dialog.Message(result);
+	} catch (e) {
+		Dialog.Message(e.StatusCode + ": " + e.Message);
+	}
+
+}
+
+function HttpPost(address, query, text) {
+	try {
+		request = CreateRequest(address);
+		var result = request.Post(query, text);
+		Dialog.Message(result);
+	} catch (e) {
+		Dialog.Message(e.StatusCode + ": " + e.Message);
+	}
+
+}
+
+function CreateRequest(address) {
+	var request = new HttpRequest(address);
+
+	for (var i = 0; i < headers.length; i++) {
+		var h = headers[i];
+		request.AddHeader(h[0], h[1]);
+	}
+
+	request.UserName = "demosr";
+	request.Password = "demosr";
+	return request;
+}
