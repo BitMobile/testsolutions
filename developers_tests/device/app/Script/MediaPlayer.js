19967d3f5619b022158127a7b2d3842b7d07ffc0
Kugushev Aleksandr <kugushew@gmail.com>
2015-06-30 17:01:25 +0300

    [PLA-430] Add tests for MediaPlayer

---
+++ developers_tests/device/app/Script/MediaPlayer.js
@@ -0,0 +1,15 @@
+function Play(){
+	$.mediaPlayer.Play();
+}
+
+function Pause(){
+	$.mediaPlayer.Pause();
+}
+
+function Stop(){
+	$.mediaPlayer.Stop();
+}
+
+function Change(){
+	$.mediaPlayer.Path = "//shared//clip.mp4";	
+}
